9a05bece7e02c5d575040b387eef146d9e116b4f
Andy Southgate <andy@mushware.com>
2006-06-06 10:29:50 +0000

    Ruby texture definitions

--- ruby/MushGLTexture.rb
+++ ruby/MushGLTexture.rb
@@ -1,10 +1,6 @@
 
 class MushGLTexture
   def MushGLTexture.cDefine(paramHash)
-    puts "Defining texture"
-    paramHash.each { |key, value|
-      puts "#{key} = #{value}"
-    }
     cRubyDefine(paramHash);
   end
 end
