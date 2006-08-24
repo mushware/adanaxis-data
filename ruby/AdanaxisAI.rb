1ddf6fdc8299fb92f870c9e42b1f8dc55281d17e
Andy Southgate <andy@mushware.com>
2006-08-24 09:58:51 +0000

    File addition

---
+++ ruby/AdanaxisAI.rb
@@ -0,0 +1,29 @@
+#%Header {
+##############################################################################
+#
+# File data-adanaxis/ruby/AdanaxisAI.rb
+#
+# Copyright Andy Southgate 2006
+#
+# This file may be used and distributed under the terms of the Mushware
+# software licence version 1.1, under the terms for 'Proprietary original
+# source files'.  If not supplied with this software, a copy of the licence
+# can be obtained from Mushware Limited via http://www.mushware.com/.
+# One of your options under that licence is to use and distribute this file
+# under the terms of the GNU General Public Licence version 2.
+#
+# This software carries NO WARRANTY of any kind.
+#
+##############################################################################
+#%Header } ZJhgffsl43t4RqQcN4aPag
+# $Id$
+# $Log$
+
+module AdanaxisAI
+  AISTATE_INVALID=0
+  AISTATE_NONE=1
+  AISTATE_DORMANT=2
+  AISTATE_IDLE=3
+  AISTATE_PATROL=4
+  AISTATE_SEEK=5
+end
