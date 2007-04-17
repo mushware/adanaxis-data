725f838f5c6367f4dafd4b86986575bf2d80f750
Andy Southgate <andy@mushware.com>
2007-04-17 11:37:40 +0000

    Graphics fix

--- spaces/level1/space.rb
+++ spaces/level1/space.rb
@@ -3,19 +3,26 @@
 #
 # File data-adanaxis/spaces/level1/space.rb
 #
-# Copyright Andy Southgate 2006-2007
+# Author Andy Southgate 2006-2007
 #
-# This file may be used and distributed under the terms of the Mushware
-# Commercial Software Licence version 1.2.  If not supplied with this software
-# a copy of the licence can be obtained from Mushware Limited via
-# http://www.mushware.com/.
+# This file contains original work by Andy Southgate.  The author and his
+# employer (Mushware Limited) irrevocably waive all of their copyright rights
+# vested in this particular version of this file to the furthest extent
+# permitted.  The author and Mushware Limited also irrevocably waive any and
+# all of their intellectual property rights arising from said file and its
+# creation that would otherwise restrict the rights of any party to use and/or
+# distribute the use of, the techniques and methods used herein.  A written
+# waiver can be obtained via http://www.mushware.com/.
 #
 # This software carries NO WARRANTY of any kind.
 #
 ##############################################################################
-#%Header } ymklWNnUsMW6L2JnkUUK9g
-# $Id: space.rb,v 1.3 2007/04/16 18:50:58 southa Exp $
+#%Header } PLAvud4jpjmgOe02d1PwsQ
+# $Id: space.rb,v 1.4 2007/04/17 10:08:12 southa Exp $
 # $Log: space.rb,v $
+# Revision 1.4  2007/04/17 10:08:12  southa
+# Voice work
+#
 # Revision 1.3  2007/04/16 18:50:58  southa
 # Voice work
 #
