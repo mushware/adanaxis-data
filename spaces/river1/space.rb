80f8cf0a1a04b9de4f1849d2264fb64551648f5e
Andy Southgate <andy@mushware.com>
2007-06-29 12:05:00 +0000

    Mandriva packaging

--- spaces/river1/space.rb
+++ spaces/river1/space.rb
@@ -123,8 +123,29 @@
 # This software carries NO WARRANTY of any kind.
 #
 ##############################################################################
-# $Id: space.rb,v 1.15 2007/06/28 14:04:58 southa Exp $
+#%Header } 0/+/WU9nlN6gOxSaFqE2Cw
+#%Header {
+##############################################################################
+#
+# File data-adanaxis/spaces/river1/space.rb
+#
+# Copyright Andy Southgate 2006-2007
+#
+# This file may be used and distributed under the terms of the Mushware
+# Software Licence version 1.4, under the terms for 'Proprietary original
+# source files'.  If not supplied with this software, a copy of the licence
+# can be obtained from Mushware Limited via http://www.mushware.com/.
+# One of your options under that licence is to use and distribute this file
+# under the terms of the GNU General Public Licence version 2.
+#
+# This software carries NO WARRANTY of any kind.
+#
+##############################################################################
+# $Id: space.rb,v 1.16 2007/06/28 15:15:13 southa Exp $
 # $Log: space.rb,v $
+# Revision 1.16  2007/06/28 15:15:13  southa
+# Mandriva fixes
+#
 # Revision 1.15  2007/06/28 14:04:58  southa
 # Mandriva packaging
 #
